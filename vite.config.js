6053ff9897e2f1b7d179217c5cf1506c7ae1c465
Anand <anandraj6783@gmail.com>
2025-07-30 20:46:52 +0530

    installed nodePolyfills

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import inject from '@rollup/plugin-inject';
-import copy from 'rollup-plugin-copy';
-import path from 'path';
+import { defineConfig } from "vite";
+import nodePolyfills from "vite-plugin-node-polyfills";
+import copy from "rollup-plugin-copy";
+import path from "path";
 
 export default defineConfig({
   define: {
@@ -10,15 +10,13 @@ export default defineConfig({
   },
 
   plugins: [
-    // Provide `process` polyfill
-    inject({ process: 'process/browser' }),
-
+    nodePolyfills({
+      protocolImports: true,
+    }),
     // Copy all assets/**/* into build/
     copy({
-      targets: [
-        { src: 'assets/**/*', dest: 'build/assets' }
-      ],
-      hook: 'writeBundle',
+      targets: [{ src: "assets/**/*", dest: "build/assets" }],
+      hook: "writeBundle",
     }),
   ],
 
@@ -28,18 +26,18 @@ export default defineConfig({
   },
 
   build: {
-    outDir: 'build',
+    outDir: "build",
     sourcemap: true, // equivalent to eval-source-map
 
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
+      input: path.resolve(__dirname, "index.html"),
       output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
       },
       manualChunks(id) {
-        if (id.includes('node_modules')) return 'vendor';
+        if (id.includes("node_modules")) return "vendor";
       },
     },
   },
