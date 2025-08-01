5ea7c06e94ef205d54eecbbd1d9e24def1b38f2d
Anand <anandraj6783@gmail.com>
2025-08-01 14:35:26 +0530

    made it a child class

--- src/entities/Birdman.js
+++ src/entities/Birdman.js
@@ -1,27 +1,11 @@
-import Phaser from "phaser";
-import collidable from "../mixins/collidable";
 
-class Birdman extends Phaser.Physics.Arcade.Sprite {
+import Enemy from "./Enemy";
+
+class Birdman extends Enemy {
   constructor(scene, x, y) {
     super(scene, x, y, "birdman");
-
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
-
-    Object.assign(this, collidable);
-
-    this.init();
-  }
-  init() {
-    this.gravity = 500;
-    this.speed = 150;
-    this.setGravityY(this.gravity);
-    this.setSize(20,46)
-    this.setOffset(7,20);
-    this.setCollideWorldBounds(true);
-    this.setOrigin(0.5, 1);
-    this.setImmovable(true);
   }
 }
 
 export default Birdman;
+ 
